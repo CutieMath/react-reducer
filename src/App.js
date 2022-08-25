3a1e2930e96234c0429b1ff43ab332f58079d21d
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-25 16:10:50 +1000

    use dispatch to update the state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import { useState, useReducer } from "react";
 import "./App.css";
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  return { count: state.count + 1 };
+};
 
 const App = () => {
+  // Dispatch will call the "action"
+  // "state" will be updated
+  // reducer function returns the updated state
   const [state, dispatch] = useReducer(reducer, { count: 0 });
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    // setCount(count + 1);
+    dispatch();
   }
 
   function decrement() {
-    setCount(count - 1);
+    // setCount(count - 1);
   }
   return (
     <div className="App">
       <button onClick={decrement}>-</button>
-      <span>{count}</span>
+      <span>{state.count}</span>
       <button onClick={increment}>+</button>
     </div>
   );
